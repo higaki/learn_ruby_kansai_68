c53033df026e89ca5085a8329308860317dfda35
HIGAKI Masaru <mas.higa@gmail.com>
2015-08-09 06:44:40 +0900

    example

--- ex1.rb
+++ ex1.rb
@@ -1,4 +1,18 @@
 require_relative 'person'
 
 class Person
-  ...
+  attr_accessor :height, :weight
+
+  protected :weight
+end
+
+if $0 == __FILE__
+  kazuhiko = Person.new('kazuhiko', Time.local(1972, 5, 8))
+  kazuhiko.height = 188
+  kazuhiko.weight =  59
+
+  kazuhiko.age                  # => 43
+  kazuhiko.height               # => 188
+  kazuhiko.weight               # => 
+end
+# ~> -:16:in `<main>': protected method `weight' called for kazuhiko(43):Person (NoMethodError)
--- ex3.rb
+++ ex3.rb
@@ -2,4 +2,23 @@ require_relative 'ex2'
 
 class Person
   def <=> o
-    ...
+    return nil unless o.kind_of? Person
+    [@name.upcase, @born] <=> [o.name.upcase, o.born]
+  end
+end
+
+if $0 == __FILE__
+  matz = Person.new('matz', Time.local(1965, 4, 14))
+
+  matz_only_name  = Person.new('matz')
+  matz_capitalize = Person.new('Matz', Time.local(1965, 4, 14))
+  matz_young      = Person.new('matz', Time.local(2000, 4, 14))
+  matz_fat        = Person.new('matz', Time.local(1965, 4, 14))
+  matz_fat.weight = 99
+
+  matz <=> matz_only_name       # => nil
+  matz <=> matz_capitalize      # => 0
+  matz <=> matz_young           # => -1
+  matz <=> matz_fat             # => 0
+  matz <=> 'matz'               # => nil
+end
